9091425cda76131e7bc217e0f30d03c44c8b2f36
Tomek <tomaszabramowski0@gmail.com>
2021-07-02 21:14:34 +0200

    fluid and absolute works

--- frontendmentor/bookmark-landing-page-master/js/myJS.js
+++ frontendmentor/bookmark-landing-page-master/js/myJS.js
@@ -1,56 +1,37 @@
-const navToggle = document.querySelector('.nav__toggle');
+// przyciski
+const navBarButton = document.getElementById('navbar__button');
+const navMenuButton = document.getElementById('nav__menu--button');
 
 
-// const navImg = document.querySelector('.nav__menu');
-let nav = document.getElementById('nav__menu--first');
-let hideNav = document.querySelector('.nav__cta');
-// let navImg = document.getElementById('nav__menu--second');
-let nav__listButton = document.getElementById('nav__menu--second');
-let navList = document.querySelector('.nav__list');
-
-
-
-const submenu = document.getElementById('nav__menu--second')
-
-// const menuHamburger = "/images/icon-hamburger.svg";
-// const menuClosed = "/images/icon-close.svg";
-
-
-
-// const menuHamburger = "../icon-hamburger.4d46a600.svg";
-// const menuClosed = "../icon-close.672c8e2b.svg";
+// kontenery
+const navbarToggle =document.querySelector('.navbar--toggle');
+const navMenu = document.querySelector('.nav__menu');
 
 
 const menuHamburger = "http://localhost:1234/icon-hamburger.4d46a600.svg";
 const menuClosed = "http://localhost:1234/icon-close.672c8e2b.svg";
 
-// PO CO ZMIENIAC IKONY SKORO I TAK ICH NIE BEDZIE WWWWWWWWIIIIIIIIIIIDDDDDDDDDDDDDDDDDDDAAAAAAAAAAAAAC
-// KURRRRRRRWWWWWWW
-
-navToggle.addEventListener('click',()=>{
-
+navBarButton.addEventListener('click',()=>{
+ 
 //  nav = (nav.src === menuHamburger) ? nav.src = menuClosed : nav = menuHamburger;
 
 //  pokaz liste 'nav__menu--second'
- navList.classList.toggle('nav--visible');
+navbarToggle.classList.toggle('nav--visible');
  //schowaj menu glowne ('.nav__cta');
- hideNav.classList.toggle('nav--visible');
+ navMenu.classList.toggle('nav--visible');
 
 })
 
-// nav__menu--second'
-nav__listButton.addEventListener('click', ()=> {
-    
-    // nav = (nav.src === menuHamburger) ? nav.src = menuClosed : nav = menuHamburger;
-
-  //  pokaz liste 'nav__menu--second'
- navList.classList.toggle('nav--visible');
- //schowaj menu glowne ('.nav__cta');
- hideNav.classList.toggle('nav--visible');
+navMenuButton.addEventListener('click', ()=>
+{
+  navbarToggle.classList.toggle('nav--visible');
+  navMenu.classList.toggle('nav--visible');
+})
 
 
 
-});
+
+
 
 //dziala
 //const navToggleAll = document.querySelectorAll('.nav__toggle');
